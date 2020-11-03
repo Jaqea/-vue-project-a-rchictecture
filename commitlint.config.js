de962ac9b15b2fc054a3762368929fd9b3caac08
Jaqea <1601676692@qq.com>
2020-11-03 16:57:25 +0800

    test: 第一次测试提交

--- commitlint.config.js
+++ commitlint.config.js
@@ -6,7 +6,7 @@ module.exports = {
     "type-enum": [
       2,
       "always",
-      ["upd", "feat", "fix", "refactor", "docs", "chore", "style", "revert"],
+      ["test", "upd", "feat", "fix", "refactor", "docs", "chore", "style", "revert"],
     ],
     "header-max-length": [0, "always", 72],
   },
